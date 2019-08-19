d73b70bc1cc1f2f35383c418fcdf8c1104a07793
AAA <absszero@gmail.com>
2019-08-19 14:33:36 +0800

    fix deprecated

--- linter.py
+++ linter.py
@@ -43,13 +43,14 @@ def _find_configuration_file(file_name):
 class PhpCsFixer(Linter):
     """Provides an interface to php-cs-fixer."""
 
-    syntax = ('php', 'html')
-    cmd = None
+    defaults = {
+        'selector': 'source.php, text.html.basic'
+    }
     config_file = '.php_cs'
-    executable = 'php-cs-fixer'
+
     regex = (
         r'^\s+\d+\)\s+.+\s+\((?P<message>.+)\)[^\@]*'
-        r'@@\s+\-\d+,\d+\s+\+(?P<line>\d+),\d+\s+@@'
+        r'\@\@\s+\-\d+,\d+\s+\+(?P<line>\d+),\d+\s+\@\@'
         r'[^-+]+[-+]?\s+(?P<error>[^\n]*)'
     )
     multiline = True
@@ -67,22 +68,23 @@ class PhpCsFixer(Linter):
 
     def cmd(self):
         """Read cmd from inline settings."""
-        settings = Linter.get_view_settings(self)
 
-        if 'cmd' in settings:
-            command = [settings.get('cmd')]
+        if 'cmd' in self.settings:
+            logger.warning('The setting `cmd` has been deprecated. '
+                           'Use `executable` instead.')
+            command = [self.settings.get('cmd')]
         else:
-            command = [self.executable_path]
+            command = ['php-cs-fixer']
 
-        if 'config_file' in settings:
-            config_file = settings.get('config_file')
+        if 'config_file' in self.settings:
+            config_file = self.settings.get('config_file')
         else:
             config_file = _find_configuration_file(self.view.file_name())
             if not config_file:
                 config_file = self.config_file
 
         command.append('fix')
-        command.append('@')
+        command.append('${temp_file}')
         command.append('--dry-run')
         command.append('--diff')
 
